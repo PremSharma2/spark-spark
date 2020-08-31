bdb22d16858aaf77e8f150cea077d6028f634169
premkaushik2 <premkaushik4@gmail.com>
2020-08-31 18:51:41 +0100

    pimping-typeEnrichment-valueClasses-implicit conversion

--- src/main/scala/typeClasses/PimpMyLibrary.scala
+++ src/main/scala/typeClasses/PimpMyLibrary.scala
@@ -15,6 +15,24 @@ object PimpMyLibrary  extends App {
  you are actually using the underlying simple type. So, that means that your instances of the wrapper classes
      creator, which means less initialization,
    increase performance and create less memory usage, because there is no instance of the wrapper classes.
+   Value classes are mostly used for performance optimization and memory optimization.
+   You can think of many of these classes as your Scala typical primitive,
+   like classes Int, Boolean,  Double, etc.
+   Use cases where you would want to and where you could apply value classes is for tiny types.
+    Let’s look at a couple of examples of how that can be applied.
+      case class UserId(id: Int)
+case class Username(name: String)
+case class TweetId(id: Int)
+case class TweetContent(content: String)
+case class User(id: UserId, name: Username)
+case class Tweet(id: TweetId, content: TweetContent)
+
+Now, what happens when I used the concept of Java types to use more specific
+classes for the user ID and username? This means that,
+now, I can’t use any number directly and can’t mix a tweet ID with a user ID
+because the compiler is helping us to verify that. So,
+that is why we created four classes here to wrap around our very simple Int and String values.
+So, User and Tweet takes tiny types instead of Int or String values directly.
    */
  implicit class RichInt(val value :Int) extends AnyVal {
     def isEven:Boolean= value%2==0
@@ -30,14 +48,25 @@ object PimpMyLibrary  extends App {
 
 
   // for example scala also uses this feature
-  // here to is aslo method of scala RichInt implicit calss method
+  // here to is also method of scala RichInt implicit class method
   // it will return a seq of 1......10 numbers in it
-  1 to 10
+  val seq: Seq[Int] =1 to 10
+  val seq2: Seq[Int] =1.to(10)
 
 
   // or one more example of scala implicit class and pimping is scala duration
 
   import scala.concurrent.duration._
   3.seconds
+
+  // One imp point compiler does not do implicit searches
+  implicit class RicherInt(richInt:RichInt){
+    def isOdd:Boolean = richInt.value%2!=0
+  }
+// so we cant call like 42.isOdd
+  // because compiler initially when search for value implicit class for
+  //42 it will find RichInt but you are asking on that RichInt search for one more implicit value class
+  // that is not possible i.e you are asking search for implicit value class for value implicit class
+
 
 }
---
+++ src/main/scala/typeClasses/PimpMyLibraryExercise.scala
@@ -0,0 +1,57 @@
+package typeClasses
+
+object PimpMyLibraryExercise extends App {
+  //Exercise 1
+// implicit && value class
+  implicit class RichString(value : String){
+  def asInt:Int = Integer.valueOf(this.value) // here this expression returns java.lang.Int which scala convert
+  //to Int
+ def encrypt(cypherDistance:Int):String = this.value.map(char => (char + cypherDistance).asInstanceOf[Char] )
+  }
+
+
+  println("3".asInt + 4)
+  println("Prem".encrypt(2))
+
+  // Exercise 2
+
+  implicit class RichInt(val value :Int) extends AnyVal {
+    def times(function: () => Unit): Unit ={
+      def timesAux(n:Int):Unit=
+        if (n<=0) ()
+        else {
+          function.apply()
+          timesAux(n-1)
+        }
+      timesAux(4)
+    }
+    def *[T](list:List[T])= {
+      def concatenate(n:Int):List[T]= {
+        if(n<=0) List()
+        else concatenate(n-1) ++ list
+      }
+      concatenate(value)
+    }
+
+
+  }
+  3.times(() => println("Scala Rocks"))
+  println(4* List(1,2))
+  implicit def stringToInt(str:String)=Integer.valueOf(str)
+  // Here compiler will search for all implicit value classes which takes value as string and look for
+  // method / defined for this , if not found look for plane value classes and inside the class look for
+  // methods which does implicit conversion of string to int or search locally
+  // and Int has / this method defined
+  //
+  //println("6" / 2)
+// implict calsses norammly are plane classes with method defined inside the class for implicit conversion
+  // This implementation is an alternate to above implementation of implicit value classes
+  class RichAltInt(value:Int){
+    implicit def implicitconversion(value:Int):RichAltInt= new RichAltInt(value)
+  }
+// Doing implicit conversions or pimping is not encouraged via method in scala i.e method implicit conversion
+  // is not encouraged lets proof it why?
+  implicit def intToBoolean(i:Int):Boolean= i==1
+val conditionedValue= if (3) "OK" else "Something Wrong"
+  println(conditionedValue)
+}
