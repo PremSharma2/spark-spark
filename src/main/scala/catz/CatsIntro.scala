f83f6f72a829c641062f8a5cd470b0ac4559ae87
premkaushik2 <premkaushik4@gmail.com>
2020-11-09 13:45:48 +0000

    Cats API [wip]

--- src/main/scala/catz/CatsIntro.scala
+++ src/main/scala/catz/CatsIntro.scala
@@ -1,42 +1,45 @@
 package catz
 
-// In built type classes
+// TODO : -> In built type classes provided by cats library
 object CatsIntro extends App {
 
-/// this is Eq type class here working behind the scene
+// TODO : -> this is Eq type class here working behind the scene
   val aComparison= 2 == "aString"
-//lets import the type class
-  // part 1 import the type class  Eq these type classes are scala in built
-  //import cats.Eq  : -. Type class
+  //TODO : -> Steps to use type Class Api of cats
+// TODO : -> Step 1:  lets import the type class
+  // TODO : ->  import the type class  Eq these type classes are scala in built
+  // TODO : -> import cats.Eq  : -. Type class
   /*
   Eq type class looks like this
-  trait Eq[A] extends Any with Serializable { self =>
+  trait Eq[A]{
   def eqv(x: A, y: A): Boolean
+  }
    */
-  //----------------------------------------------------------------------
+  // TODO : -----------------------------------------------------------------------
 
-  // part2 import implicit  type class instances
-  // for a particular type i.e Int here
-//--------------------------------------------------------------------------
+  // TODO : -> Step 2:  import implicit  type class instances
+  //  TODO : -> for a particular type i.e Int here
+// TODO : ---------------------------------------------------------------------------
 
-// part 3 use the type class API
+// TODO : -> Step  3 use the type class API
   /*
-  Here Eq comanion object looks like this
+   TODO : -> Here Eq companion object looks like this
 
   it takes type class instance as implicit value Like we did in
-  Json Serilization way
+   TODO : -> Json Serialization way
   and this apply returns the type class instance
    */
   /*
-  Companion object looks like this
-  This is ours and below one is scala one
+  TODO : ->
+   TODO : -> Companion object looks like this
+ TODO : ->  This is ours and below one is scala one
   object Equal{
     // this takes an type class instance
     // we ca read like this use this equalizer on these values of Type T
     def apply[T](a:T,b:T)(implicit equalizer:Equal[T]) = equalizer
   }
   // This is Type Class instance
-  implicit object NameEquality extends Equal[User] {
+  implicit object    User   extends Equal[User] {
     override def apply(a: User, b: User): Boolean = a.name==b.name
    */
   /*
@@ -44,12 +47,12 @@ object CatsIntro extends App {
 final def apply[A](implicit ev: Eq[A]): Eq[A] = ev
    */
 
-// this will automatically inject implicit type class instance
-  // of type int into the Eq companion because we have given the type is Int
+// TODO : -> this will automatically inject implicit type class instance
+  //TODO : ->  of type int into the Eq companion because we have given the type is Int
   import cats.Eq
   import cats.instances.int._
   // Here we are calling Eq type class companion object which in turn returns the
-  // Type class instance
+  // TODO : Type class instance
   val intEqualityTypeClassInstance: Eq[Int] = Eq.apply[Int]
   /*
   now we will call eqv method of type class Eq via type class instance
@@ -57,13 +60,13 @@ final def apply[A](implicit ev: Eq[A]): Eq[A] = ev
     ev.eqv(x, y)
 
    */
-
+  // TODO : calling eqv def over typeclass instance
    intEqualityTypeClassInstance.eqv(2,2)
   /*
   Like this : ->
-
+//TODO : This example making  us understand that how scala Work internally
   trait Equal[T]{
-    def apply(a:T,b:T):Boolean
+    def eqv(a:T,b:T):Boolean
   }
   //Companion object for equal
   object Equal{
@@ -73,33 +76,41 @@ final def apply[A](implicit ev: Eq[A]): Eq[A] = ev
   }
   // This is Type Class instance
   implicit object NameEquality extends Equal[User] {
-    override def apply(a: User, b: User): Boolean = a.name==b.name
+    override def eqv(a: User, b: User): Boolean = a.name==b.name
    */
-
+// TODO : Equal[USer].apply.eqv(a,b)
 
   //--------------------------------------------------------------
-  // part 4 use extension methods or type enrichment by importing it
+  // TODO :  Step4 for Impl
+  //  use extension methods or type enrichment by importing it
   /*
   Like this : ->
-  type Enrichment or extension method
+ TODO: -> type Enrichment or extension method
   implicit class TypeSafeEqual[T](value:T){
     def ===(other:T)(implicit equalizer:Equal[T]) = equalizer.apply(value,other)
     def =!=(other:T)(implicit equalizer:Equal[T]) = !equalizer.apply(value,other)
   }
+  Note: But cats API uses implicits defs and implicits vals and we are using
+  implicits classes
+
    */
   import cats.syntax.eq._
   val a: Boolean = 2===3
 
-  // part 5 Extending type class operation to composite types
-  // for that we need to import the correct implicit  type  class instance
+  // TODO : -> part 5 Extending type class operation to composite types
+  // TODO : -> for that we need to import the correct implicit  type  class instance
   import cats.instances.list._ // by this we bring Eq[List[Int]]
   val aListcomparison: Boolean = List(2) === List(3)
 
-  // part 6 create a type class instance for custom type
+// TODO -----------------------------------------------------------------------------------
+
+  // TODO : Exercise  create a type class instance for custom type
   case class ToyCar(model :String, price:Double)
   // create your own type class using cats API
-  // now what scala will  do here it will create a type class instance
-  //i.e Eq[A] instance but it is not singleton object it is anonymous class object
+  // TODO : now what scala will  do here it will create a type class instance
+  //TODO :i.e Eq[A].instance(f)(implicit typeclass object) def
+  // but it is not singleton object
+  // it is anonymous class object
   // using this function f: (A, A) => Boolean
   //i mean its an way of creating type class instance for custom type
   // with this method scala will give you type class instance
--- src/main/scala/catz/SemiGroups.scala
+++ src/main/scala/catz/SemiGroups.scala
@@ -1,10 +1,11 @@
 package catz
 
 import cats.Semigroup
-//Semigroup is a type class that combines the Element of same type
+// TODO : -> Semigroup is a type class that combines the Element of same type
 // and what it combines that depends on type class instance
 /*
-SemiGroup Type class Looks like that
+TODO : SemiGroup Type class Looks like that
+
 trait Semigroup[@sp(Int, Long, Float, Double) A] {
 
  Associative operation taking which combines two values.
@@ -14,56 +15,117 @@ trait Semigroup[@sp(Int, Long, Float, Double) A] {
 
 //lets import the type class
 object SemiGroups  extends App {
-  // lets import type class instances
+  // TODO lets import type class instances for Int type
   import cats.instances.int._
   // naturalIntSemiGroupTypeClassInstance is an type class instance
   val naturalIntSemiGroupTypeClassInstance: Semigroup[Int] = Semigroup.apply[Int]
+
+  //TODO calling combine operation on type class instance
+
   val combineNaturals: Int = naturalIntSemiGroupTypeClassInstance.combine(2,46)
   println(combineNaturals)
 
   // naturalStringSemiGroupTypeClassInstance is an type class instance
-  // import type class instance
+  // TODO import type class instance for the String type
   import cats.instances.string._
   val naturalStringSemiGroupTypeClassInstance: Semigroup[String] = Semigroup.apply[String]
   val combineNaturalStrings= naturalStringSemiGroupTypeClassInstance.combine("Jai","Maa")
   println(combineNaturalStrings)
 
+  // TODO --------------------------------------------------------------------------------
 
   // TODO : -> use of Semigroup type class we created an API
-  //TODO : -> this we can enhance by converting into type Enrichment by making it implicit class
+  //TODO : -> this we can enhance by converting into
+  // type Enrichment by making it implicit class
   //TODO : -> General APi for Reduction of any type using type class
 
   def reduceThings[T](list:List[T])(implicit semiGroup:Semigroup[T]) = {
-    list.reduce(semiGroup.combine)
+    list.reduce(semiGroup combine)
     //
   }
 
 // TODO : -> These are specific APIS over here we dont require combine function
 
 val fx : (Int,Int) => Int = (x,y) => x+ y
+
   def   reduceInts(list:List[Int]) = list.reduce(fx)
 
   def   reduceStrings(list:List[String]) = list.reduce(_ + _)
+
   def sum (x:Int,y:Int): Int= x+y
+
   List(1,2,3,4).reduce(sum)
+
     print(reduceInts(List(1,2,3,4)))
+
+  // TODO : -> by using Reduce things API
+
   val myList= (1 to 4).toList
+
   println(reduceThings(myList))
+
+  // TODO : -> converting List[Int] to List[Option[Int]]
+
   val numberOptions: List[Option[Int]] = myList.map(Option(_)).toList
-  // importing the Semigroup type class instance of Type Option[Int]
+
+  // TODO :-> importing the implicit  Semigroup type class instance of Type Option[Int]
   import cats.instances.option._
-  println(reduceThings(numberOptions))// it will sum all elements and wrap them in Monad Option
+  // TODO : -> it will sum all elements of List  and wrap them in Monad Option
+  println(reduceThings(numberOptions))
 val strings= List("I am" , "starting" ,"to like " , "SemiGroup")
 
   val stringOption: List[Option[String]] = strings.map(Option(_)).toList
   println(reduceThings(stringOption))// It will concat the list and wrap them Option Monad
   //TODO --------------------------------------------------------------------------------------
-  // TODO 1: Exercise make this api to support for Custom Types This Impl
+  // TODO 1: Exercise make this api to support for Custom Types
 
   case class Expense(id:Long, amount:Double)
-  // TODO : create your Type class Instance for Expense Type which is custom Type Expense
+  // TODO : creating the  Type class Instance for Expense Type which is custom Type Expense
+  // TODO and marking it implicit
   implicit val typeClassInstanceExpense = Semigroup.instance[Expense]{
     (e1,e2) => Expense(Math.max(e1.id,e2.id), e1.amount + e2.amount)
   }
+
+  val expenses= List(Expense(1,60),Expense(2,70), Expense(3,80), Expense(4,90))
+
+  println(reduceThings(expenses))
+
+  //TODO : -> USe of type Enrichment or Extension Methods from Semi Group , or pimping
+  // lets import the correct package
+  import cats.implicits.catsSyntaxSemigroup
+  /*
+  TODO : type Enrichmet by this implcit def catsSyntaxSemigroup
+   TODO : It will inject the desired implicit type class instance of type Class SemiGroup
+ TODO : i.e 2 will be converted into val enrichment= SemigroupOps(implicit typeclassinstance)(2)
+  TODO : then  compiler will call enrichment.|+|(3), hence it is implicitly enriched
+  trait SemigroupSyntax {
+
+  implicit final def catsSyntaxSemigroup[A: Semigroup](a: A): SemigroupOps[A] =
+    new SemigroupOps[A](a)
+}
+
+final class SemigroupOps[A: Semigroup](lhs: A) {
+  def |+|(rhs: A): A = macro Ops.binop[A, A]
+  def combine(rhs: A): A = macro Ops.binop[A, A]
+  def combineN(rhs: Int): A = macro Ops.binop[A, A]
+}
+   */
+  val anIntsumUsingTypeEnrichMent = 2 |+| 3
+
+  val anStringConcatUsingTypeEnrichMent = "JAI" |+| "Mata"
+
+  println(anIntsumUsingTypeEnrichMent)
+  println(anStringConcatUsingTypeEnrichMent)
+  val aCombinedExpense: Expense = Expense(1,60) |+| Expense(2,40)
+  println(aCombinedExpense)
+  // TODO: implement reduce things API with |+|
+  //TODO : here we passed Lambda combiner function fx : (x,y) => x |+| y
+  //TODO : here x is implcitly converted to implict class refrence
+  //TODO on top of that refrence we will call |+| method
+  def reduceThings1[T](list:List[T])(implicit semiGroup:Semigroup[T]) = {
+    list.reduce(_ |+| _)
+  }
+
+println(reduceThings1(expenses))
 
 }
