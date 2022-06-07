20c982f91bc13ec837045c81c3fd41cc4d5825b1
Prem Sharma <32658902+PremSharma2@users.noreply.github.com>
2022-06-07 09:21:51 +0100

    Zio Exception handling

--- src/main/scala/zio/Zio.scala
+++ src/main/scala/zio/Zio.scala
@@ -65,12 +65,20 @@ val result2 = println("The meaning of life is 42")
     // data structures to wrap a value or an error
    // the input type is "any", since they don't require any input
    */
-
+//TODO : Effects that cant Fail
+//TODO :  type UIO[+A]= ZIO[Any, Nothing, A]  // Succeed with an `A`, cannot fail , no requirements.
+/*
+TODO
+    Since We can never produce a value of type nothing We know  that
+    a computation with an Error type of Nothing can never Fail
+    because Nothing represents nothing returned
+    UIO[+A]= ZIO[Any, Nothing, A]
+ */
   val zmol:ZIO[Any,Nothing,Int] = ZIO.succeed(42)
   //type IO[+E, +A]   = ZIO[Any, E, A] // Succeed with an `A`, may fail with `E`  , no requirements.
   //Returns an effect that models failure with the specified error. The moral equivalent of throw for pure code
   //def fail[E](error: => E): IO[E, Nothing]
-  // type IO[+E, +A]   = ZIO[Any, E, Nothing]  // Succeed with an `A`, may fail with `E`, no requirements
+  // type IO[+E, +A]   = ZIO[Any, Exception, Nothing]  // Succeed with an `A`, may fail with `E`, no requirements
   val fail: IO[String, Nothing] = ZIO.fail("Something went wrong") // notice the error can be of any type
 
   zmol.exitCode
@@ -134,7 +142,7 @@ TODO Understanding map and FlatMap
   // followed by the passing of its value to the specified continuation function k,
   // followed by the effect that it returns.
   //val parsed = readFile("foo.txt").flatMap(file => parseFile(file))
-
+    OR  We can say that combining the Effects Sequentially
 
 
 
@@ -143,16 +151,23 @@ TODO Understanding map and FlatMap
 val readLine1: ZIO[Any, Throwable, String] = ZIO.effect(StdIn.readLine())
   val echo: ZIO[Any, Throwable, Unit] = readLine1.flatMap(line => printLine(line))
   /*
+
+
+
+
 Todo
   map of ZIO
   Returns an effect whose success is mapped by the specified f function.
   def map[B](f: A => B): ZIO[R, E, B] = new ZIO.FlatMap(self, new ZIO.MapFn(f))
   new ZIO.FlatMap(self, new ZIO.MapFn(f))
+ final class FlatMap[R, E, A, B](val zio: ZIO[R, E, A], val k: A => ZIO[R, E, B])
+TODO
    final class MapFn[R, E, A, B](override val underlying: A => B) extends ZIOFn1[A, ZIO[R, E, B]] {
     def apply(a: A): ZIO[R, E, B] =
       new ZIO.Succeed(underlying(a))
   }
-   private[zio] final class FlatMap[R, E, A0, A](val zio: ZIO[R, E, A0], val k: A0 => ZIO[R, E, A])
+  abstract class ZIOFn1[-A, +B] extends ZIOFn with Function[A, B]
+
    */
   val mapZio: ZIO[Any, Throwable, String] =readLine1.map(_.toUpperCase)
   /*
@@ -180,9 +195,95 @@ TODO
     _ <- preparingCoffee.debug(printThread)
   } yield ()
 
+/*
+TODO
+     Handling Error with Zio
+     You can surface failures with ZIO#either,
+     which takes an ZIO[R, E, A] and produces an ZIO[R, Nothing, Either[E, A]].
+     Returns an effect whose failure and success have been lifted into an Either.
+     The resulting effect cannot fail,
+     because the failure case has been exposed as part of the Either success case.
+    This method is useful for recovering from ZIO effects that may fail.
+   The error parameter of the returned ZIO is Nothing,
+   since it is guaranteed the ZIO effect does not model failure.
+ */
+//type UIO[Either]      = ZIO[Any, Nothing, Either]
+  val zeither: UIO[Either[String, Int]] =
+    IO.fail("Uh oh!").either
+/*
+TODO
+    You can submerge failures with ZIO.absolve,
+    which is the opposite of either and turns an ZIO[R,
+     Nothing, Either[E, A]] into a ZIO[R, E, A]:
+     Submerges the error case of an Either into the ZIO. The inverse operation of IO.either.
+     def absolve[R, E, A](v: ZIO[R, E, Either[E, A]]): ZIO[R, E, A] =
+    v.flatMap(fromEither(_))
+ */
+   val srtOp: ZIO[Any, Nothing, Either[String, Double]] = sqrt(ZIO.succeed(2.0))
 
+  def sqrt(io: UIO[Double]) =
+      io.map(value =>
+        if (value < 0.0) Left("Value must be >= 0.0")
+        else Right(Math.sqrt(value))
+      )
+    val absoveOutput: ZIO[Any, String, Double] = ZIO.absolve(srtOp)
 
-//type URIO[-R, +A] = ZIO[R, Nothing, A]
+  /*
+  TODO
+      ZIO can preserve information
+      -We can Express how exactly our effects can fail
+      - Composed ErrorTypes Automatically inferred you can see here in readConfig def
+      -
+   */
+  sealed trait ConfigError
+  case class Config(params: String = _)
+
+  final case class IOError(message: String) extends ConfigError
+
+  final case class ParseError(message: String) extends ConfigError
+
+  def readFile(path: String): ZIO[Any, IOError, String] = ???
+
+  def parseConfig(s: String): ZIO[Any, ParseError, Config] = ???
+
+  def readConfig(path: String): ZIO[Any, ConfigError, Config] = {
+    readFile("").flatMap(_ => parseConfig(""))
+  }
+
+  /*
+TODO
+    Catching All Errors
+ If you want to catch and recover from
+ all types of errors and effectfully attempt recovery,
+  you can use the catchAll method:
+   */
+  val z: IO[ IOError, String] =
+    readFile("primary.json").catchAll(_ =>
+      readFile("backup.json"))
+
+  /*
+ TODO
+    Catching Some Errors
+   If you want to catch and recover from only some types of exceptions and effectfully attempt recovery,
+   you can use the catchSome method:
+   */
+  val data: ZIO[Any, IOError, String] =
+    readFile("primary.data").catchSome {
+      case _ : IOError =>
+        readFile("backup.data")
+    }
+
+  /*
+ TODO
+    Fallback
+  You can try one effect, or, if it fails,
+  try another effect, with the orElse combinator:
+   */
+
+  val primaryOrBackupData: ZIO[Any, IOError, String] =
+    readFile("primary.data").orElse(readFile("backup.data"))
+
+  //type URIO[-R, +A] = ZIO[R, Nothing, A]
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] ={
     sequentialWakeUpRoutine().exitCode
     //zmol.exitCode
